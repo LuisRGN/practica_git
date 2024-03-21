1ca0c223319591008f17d7bc447cdaf558ebf5b5
Carlos Molina <105681853+Crlziito04@users.noreply.github.com>
2024-03-21 18:34:50 -0300

    ValidateEmail

--- src/service/practiceLogic.js
+++ src/service/practiceLogic.js
@@ -101,3 +101,7 @@ miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020).
 20) Programa una función que valide que un texto sea un email válido, pe. 
     miFuncion("jonmircha@gmail.com") devolverá verdadero.
 */
+const validateEmail = (email) => {
+  var patron = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return patron.test(email);
+};
