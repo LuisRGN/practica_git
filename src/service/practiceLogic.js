7dc7b148b73200dc81bd0adf66767998b996b12f
LuisRGN <luisrafael1598@gmail.com>
2024-03-21 16:20:43 -0500

    ..

--- src/service/practiceLogic.js
+++ src/service/practiceLogic.js
@@ -27,7 +27,7 @@
 // Output: [2,3]
 
 /*
-otros ejercios que busque xD
+otros ejercios que busque xD ..
 
 1) Programa una función que cuente el número de caracteres de una cadena de texto, pe. 
     miFuncion("Hola Mundo") devolverá 10.
